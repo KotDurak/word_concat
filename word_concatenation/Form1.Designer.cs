e979579e4ab55645e4199ca3de70687a7552a1b8
KotDurak <DarkProkuror@mail.ru>
2019-05-22 23:02:09 +0400

    start

---
+++ word_concatenation/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace word_concatenation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addRowButton = new System.Windows.Forms.Button();
+            this.deleteRowButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(408, 196);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // addRowButton
+            // 
+            this.addRowButton.Location = new System.Drawing.Point(29, 216);
+            this.addRowButton.Name = "addRowButton";
+            this.addRowButton.Size = new System.Drawing.Size(75, 23);
+            this.addRowButton.TabIndex = 1;
+            this.addRowButton.Text = "addRow";
+            this.addRowButton.UseVisualStyleBackColor = true;
+            this.addRowButton.Click += new System.EventHandler(this.addRowButton_Click);
+            // 
+            // deleteRowButton
+            // 
+            this.deleteRowButton.Location = new System.Drawing.Point(161, 215);
+            this.deleteRowButton.Name = "deleteRowButton";
+            this.deleteRowButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteRowButton.TabIndex = 2;
+            this.deleteRowButton.Text = "deleteRow";
+            this.deleteRowButton.UseVisualStyleBackColor = true;
+            this.deleteRowButton.Click += new System.EventHandler(this.deleteRowButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 289);
+            this.Controls.Add(this.deleteRowButton);
+            this.Controls.Add(this.addRowButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addRowButton;
+        private System.Windows.Forms.Button deleteRowButton;
+    }
+}
+
---
+++ word_concatenation/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace word_concatenation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void addRowButton_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns["name"].DisplayIndex = 2;
+        }
+
+        private void deleteRowButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var column1 = new DataGridViewColumn();
+            column1.HeaderText = "Test";
+            column1.Width = 100;
+            column1.ReadOnly = true;
+            column1.Name = "name";
+            column1.Frozen = false;
+            column1.CellTemplate = new DataGridViewTextBoxCell();
+          
+
+            var column2 = new DataGridViewColumn();
+            column2.HeaderText = "Price";
+            column2.Name = "price";
+            column2.CellTemplate = new DataGridViewTextBoxCell();
+            
+            var column3 = new DataGridViewColumn();
+            column3.HeaderText = "Remainder";
+            column3.Name = "count";
+            column3.CellTemplate = new DataGridViewTextBoxCell();
+         
+            dataGridView1.Columns.Add(column1);
+            dataGridView1.Columns.Add(column2);
+            dataGridView1.Columns.Add(column3);
+
+            dataGridView1.AllowUserToAddRows = false;
+
+            for (int i = 0; i < 5; ++i)
+            {
+                //Добавляем строку, указывая значения колонок поочереди слева направо
+                dataGridView1.Rows.Add("Example 1, Good " + i, i * 1000, i);
+            }
+
+            for(int i = 0; i < 5; ++i)
+            {
+                //Добавляем строку, указывая значения каждой ячейки по имени (можно использовать индекс 0, 1, 2 вместо имен)
+                dataGridView1.Rows.Add();
+                dataGridView1["name", dataGridView1.Rows.Count - 1].Value = "Example 2 Good" + i;
+                dataGridView1["price", dataGridView1.Rows.Count - 1].Value = i * 1000;
+                dataGridView1["count", dataGridView1.Rows.Count - 1].Value = i;
+            }
+
+            //А теперь простой пройдемся циклом по всем ячейкам
+            for(int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for(int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    object o = dataGridView1[j, i].Value;
+                }
+            }
+        }
+    }
+
+}
